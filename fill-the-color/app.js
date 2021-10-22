fa5a4f920c4418da9efba161412cfead4ee33d44
akashbhandwalkar <abakash02@gmail.com>
2021-10-22 11:20:56 +0530

    Fill Grid challenge: added hover effect.

--- fill-the-color/app.js
+++ fill-the-color/app.js
@@ -1,44 +1,70 @@
+
+// DOM elemenets
 let spotterContainer = document.querySelector(".color-spotter-container");
 let colorContainer = document.querySelector(".color-container");
+
+// Global Variables
 let colorToFill = 'white';
 let SQUARE = 14;
 let gridColor = 'white';
+const COLORS = ['#f77b9c', '#3f6ab5', '#88d8b0', '#ea5486', '#854442', '#f37735', '#0f0f0f','#570384','#bc863a','#e8ba17','#fe4a49', '#eec9d2', '#451e3e', '#4a4e4d'];
+let isDragging = false;
+var totalWidth = window.innerWidth < 500 ? window.innerWidth - 20: 500;
 
-const COLORS = ['#f77b9c', '#3f6ab5', '#0a9b4e', '#ea5486', '#910004', '#8f79e0', '#0f0f0f','#570384','#bc863a','#e8ba17','#f77b9c', '#3f6ab5', '#0a9b4e', '#ea5486'];
 
-var totalWidth = 400;
-spotterContainer.style.width = totalWidth + 'px';
-spotterContainer.style.height = totalWidth + 'px';
+// Setting up container dimensions
+(function() {
+  spotterContainer.style.width = totalWidth + 'px';
+  spotterContainer.style.height = totalWidth + 'px';
+})();
 
-    
-function generateGrid(SQUARE, container, flag) {
-    container.innerHTML = null;
-    let _SQUARE = flag ? SQUARE : 1;
-    for(let i = 0; i < _SQUARE; i++) {
-        var outer = createElement('div', 'outer flex');
-        for(let j = 0; j < SQUARE; j++) {
-            var inner= createElement('div', 'inner square', true);
-            if(!flag) {
-              inner = createElement('div', 'inner color-square', true);
-              inner.style.background = COLORS[j];
-              inner.onclick = selectColor.bind(inner, j);
-            } else {
-               inner.onclick = setAnswer.bind(inner, i, j);
-               inner.onmouseover = setAnswer.bind(inner, i, j);
-            }
-            outer.append(inner);
-        }
-        container.append(outer);
-    }
+
+// Mocking drag event for onhover coloring
+spotterContainer.addEventListener('mousedown', () => {
+  isDragging = true;
+});
+
+
+spotterContainer.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+
+// genrate color grid
+function generateColorGrid(SQUARE, container) {
+  container.innerHTML = null;
+  var outer = createElement('div', 'outer flex');
+  for(let j = 0; j < SQUARE; j++) {
+      inner = createElement('div', 'inner color-square', true);
+      inner.style.background = COLORS[j];
+      inner.onclick = selectColor.bind(inner, j);
+      outer.append(inner);
+  }
+  container.append(outer);
 }
+
+
+// Generates the empty Grid
+function generateEmptyGrid(SQUARE, container, emptyBlock) {
+  container.innerHTML = null;
+  for(let i = 0; i < SQUARE; i++) {
+      var outer = createElement('div', 'outer flex');
+      for(let j = 0; j < SQUARE; j++) {
+          var inner= createElement('div', 'inner square', true);
+          inner.onclick = setColor.bind(inner, false);
+          inner.onmouseover = setColor.bind(inner, true);
+          outer.append(inner);
+      }
+      container.append(outer);
+  }
+}
+
 
 function selectColor(index) {
   const outers = document.getElementsByClassName('color-square');
   for(let index = 0; index < outers.length; index++) {
-    console.log(outers[index].classList);
     outers[index].classList.remove('active');
   }
-  debugger;
   if(colorToFill === COLORS[index]) {
     colorToFill = 'white';
   } else {
@@ -66,10 +92,12 @@ function setSquareDimension(outer) {
     return outer;
 }
 
-function setAnswer(i) {
-  this.style.background = colorToFill;
+function setColor(onhover) {
+  if((onhover && isDragging) || !onhover) {
+    this.style.background = colorToFill;
+  }
 }
 
-generateGrid(SQUARE, spotterContainer, true);
-generateGrid(SQUARE, colorContainer);
+generateEmptyGrid(SQUARE, spotterContainer, true);
+generateColorGrid(SQUARE, colorContainer);
 
